4672647e7c63f97c9854856658664b98f7396755
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-05 09:18:48 +0200

    added  styling to h1

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,4 @@
 // business logic
-// function Pizza(size,crust,topping){
-//     this.Size = size;
-//     this.Crust = crust;
-//     this.Topping = topping;
-// }
-// function Smallpizza(){
-//     var size = small;
-//     var price = 950;
-// }
-// function mediumpizza(){
-//     var size = medium;
-//     var price = 1050;
-// }
-// function largepizza(){
-//     var size = large;
-//     var price = 1550;
-// }
-// function pizzaorder(){
-//     var likesize = parseFloat(document.getElementById('pizzasize').value);
-//     var  likecrust = parseFloat(document.getElementById('pizzacrust').value);
-//     var liketopping = parseFloat(document.getElementById('pizzatoppings').value);
-// }
-// var checkedValue = document.getElementById('find').value;
 
 // UI
 $(document).ready(function(){
