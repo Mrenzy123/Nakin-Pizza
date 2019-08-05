5fc21aba4ebdce135b5f1f58f7b131c3b215d8d1
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-05 09:16:37 +0200

    added changes to script file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // business logic
-function Pizza(size,crust,topping){
-    this.Size = size;
-    this.Crust = crust;
-    this.Topping = topping;
-}
+// function Pizza(size,crust,topping){
+//     this.Size = size;
+//     this.Crust = crust;
+//     this.Topping = topping;
+// }
 // function Smallpizza(){
 //     var size = small;
 //     var price = 950;
@@ -16,39 +16,26 @@ function Pizza(size,crust,topping){
 //     var size = large;
 //     var price = 1550;
 // }
-function pizzaorder(){
-    var likesize = parseFloat(document.getElementById('pizzasize').value);
-    var  likecrust = parseFloat(document.getElementById('pizzacrust').value);
-    var liketopping = parseFloat(document.getElementById('pizzatoppings').value);
-}
+// function pizzaorder(){
+//     var likesize = parseFloat(document.getElementById('pizzasize').value);
+//     var  likecrust = parseFloat(document.getElementById('pizzacrust').value);
+//     var liketopping = parseFloat(document.getElementById('pizzatoppings').value);
+// }
 // var checkedValue = document.getElementById('find').value;
 
 // UI
 $(document).ready(function(){
     $("form").submit(function(event){
-    var likesize = parseFloat(document.getElementById('pizzasize').value);
-    var likesize1 = document.getElementById('pizzasize').name;
-    var  likecrust = parseFloat(document.getElementById('pizzacrust').value);
-    var liketopping = parseFloat(document.getElementById('pizzatoppings').value);
-    var total = (likesize + likecrust + liketopping);
-    document.getElementById("getsizehistory").innerHTML = likesize1 + ":" + likesize;
-    document.getElementById("getcrusthistory").innerHTML =   likecrust;
-    document.getElementById("gettoppinghistory").innerHTML =  liketopping;
+    var likeSize = parseFloat(document.getElementById('pizzasize').value);
+    var  likeCrust = parseFloat(document.getElementById('pizzacrust').value);
+    var likeTopping = parseFloat(document.getElementById('pizzatoppings').value);
+    var likeQuantity = parseFloat(document.getElementById('pizzaquantity').value);
+    var total = (likeSize + likeCrust + likeTopping + likeQuantity);
+    document.getElementById("getsizehistory").innerHTML =  likeSize;
+    document.getElementById("getcrusthistory").innerHTML =   likeCrust;
+    document.getElementById("gettoppinghistory").innerHTML =  likeTopping;
     document.getElementById("displaysum").innerHTML = total;
-    document
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
     event.preventDefault();
     })
 })
