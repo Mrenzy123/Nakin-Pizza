908b931e5ceeacd05ab9671e1042cc6714189a83
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-05 13:55:01 +0200

    add changes to script file.

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
         if (checkVal === "yes"){
             deliverAmount = 700;
             var getLocation = prompt("Tell your location:");
-            alert(" Pizza will be delivered at" + getLocation + "with additional amount of 700");
+            alert(" Pizza will be delivered at " + getLocation + "with additional amount of 700");
         }
     })
     $("form").submit(function(event){
